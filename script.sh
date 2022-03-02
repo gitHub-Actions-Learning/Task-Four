9957d4c0d8c598f6362423aa5ecf50c9ee3d8981
ahmed-ahmedmohamad <ahmed.ahmedmohamad@vodafone.com>
2022-03-02 11:20:08 +0200

    Update s3

--- script.sh
+++ script.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 set -e
 
-ChanagedDirs=$(git diff --name-only HEAD | awk -F'/' '{print $1"/"$2"/"$3;}' | uniq) #array of dirs
-echo $ChanagedDirs
 
+
+
+# changesCommited=$(git diff --name-only HEAD)
+# if [ "$changesCommited" != " " ]
+# then
+#     # echo $changesCommited
+#     # git add .
+#     # git commit -m "Update $module"
+#     # git push origin test
+# fi
 # for eachDir in $ChanagedDirs
 # do
 #init 
@@ -20,9 +28,11 @@ echo $ChanagedDirs
 
 
 # 2- add the whole pipeline in terraform.yml in loop
+
+ChanagedDirs=$(git diff --name-only HEAD | awk -F'/' '{print $1"/"$2"/"$3;}' | uniq) #array of dirs
+echo $ChanagedDirs
 for dir in $ChanagedDirs
 do
-
 #init 
 #plan
 #scan
@@ -48,13 +58,15 @@ then
     git add .
     git commit -m "Update $module"
     git push origin test
+else
+    continue
 fi
 
 git add .
 git tag -a $newTagName -m "Upload $newTagName" #vpc-v6 #tag types ==> light / annotate ==> full info
 git push origin $newTagName
-
 done 
+
 #more than on module vpc/s3 ==> 
 # 1- will create 2 tags for each module with new version
 # 2- will need to cd for each dir have chnages and run the whole pipeline ==> 
